93ebe492a0201a4023d0118f2751cb8714c4b027
saroj <kandelsoftware@gmail.com>
2016-08-16 21:50:29 +0200

    edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-  before_action :set_post ,except:[:index,:new]
   def index
    @posts=Post.all
   end
   def show
+    @post=Post.find(params[:id])  
+
   end
   def new
     @post=Post.new
@@ -16,21 +17,24 @@ class PostsController < ApplicationController
     render :new
   end
   end
+  def edit
+    @post=Post.find(params[:id])  
+  end
   def update
+    @post=Post.find(params[:id])  
     if @post.update(post_params)
       redirect_to @post, notice: "Post is updated"
     else
+    render :edit
     end
    end
   def destroy
+    @post=Post.find(params[:id])  
     @post.destroy
+    redirect_to  root_path
   end
 
   private
-
-  def set_post
-    @post=Post.find(params[:id])  
-  end
   def post_params
      params.require(:post).permit(:title,:body)
   end
